285c73d677f3609cf56b5b9643719c5f9f4bfdf6
ForcesR2N <muhammadzuhrizal07@gmail.com>
2025-01-07 09:39:09 +0700

    testing

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_project/page/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+      ),
+      home: LoginPage(),
+    );
+  }
+}
